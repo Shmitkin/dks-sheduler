d00ad0925929093fe5a9f5a5122eafbe4eb40da3
Shmitkin <mitkinoleg@gmail.com>
2020-12-17 14:13:06 +0300

    Создаст шаблон приоржения

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -14,30 +14,31 @@ export default function App() {
           <button className="header__autorization-button" type="button">Авторизация</button>
         </div>
         <div className="main-content-container">
+
           <div className="task-area">
-            <p>Область задач</p>
-            <ul>
-              <li>Задача1</li>
-              <li>Задача2</li>
-              <li>Задача3</li>
+            <p className="task-area__title">Область задач</p>
+            <ul className="task-area__list">
+              <li className="task-area__list-item">Задача1</li>
+              <li className="task-area__list-item">Задача2</li>
+              <li className="task-area__list-item">Задача3</li>
             </ul>
-            <button type="button">Добавить задачу</button>
+            <button className="task-area__button" type="button">Добавить задачу</button>
           </div>
 
           <div className="current-task-area">
-            <p>Область конкретной задачи</p>
-            <ul>
-              <li>Шаг 1</li>
-              <li>Шаг 2</li>
-              <li>Шаг 3</li>
+            <p className="current-task-area__title">Область конкретной задачи</p>
+            <ul className="current-task-area__list">
+              <li className="current-task-area__list-item">Шаг1</li>
+              <li className="current-task-area__list-item">Шаг2</li>
+              <li className="current-task-area__list-item">Шаг3</li>
             </ul>
-            <button type="button">Добавить шаг</button>
+            <button className="current-task-area__button" type="button">Добавить задачу</button>
           </div>
 
           <div className="monitoring-area">
-            <p>Мониторинг</p>
-            <div>График</div>
-            <div>еще какой-то график</div>
+            <p className="monitoring-area__title">Мониторинг</p>
+            <div className="monitoring-area__mock-graph">График</div>
+            <div className="monitoring-area__mock-graph">еще какой-то график</div>
           </div>
         </div>
       </div>
